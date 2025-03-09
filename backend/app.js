d9e6c9b515ccba03482ed79cdba867b1f0e68624
nanthakumar-dev <nantha656565@gmail.com>
2025-03-09 08:55:57 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,7 @@ const path=require('path')
 const app=express()
 const port=8000
 
-app.use(express.static(path.join(__dirname,'../frontend/dist')))
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../frontend/dist/index.html'))
-})
+
 
 app.listen(port,()=>{
     console.log("server Connect Successfully")
